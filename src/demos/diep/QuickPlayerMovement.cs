66c41f3db88cdd634ec46d8c2689e7bd91d64ff9
unknown <jalen.morgan000@gmail.com>
2023-08-16 10:18:47 -0400

    Add movement to diep example

---
+++ src/demos/diep/QuickPlayerMovement.cs
@@ -0,0 +1,40 @@
+using Godot;
+using GodotSteeringAI;
+using System;
+
+public partial class QuickPlayerMovement : CharacterBody2D
+{
+    [Export]
+    public float maxSpeed = 500.0f;
+
+    [Export]
+    public float userAcceleration = 500.0f;
+
+    [Export]
+    public float maxAcceleration = 200.0f;
+
+    [Export]
+    public float friction = 10.0f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 userInputAcceleration =
+            (float)delta
+            * this.userAcceleration
+            * new Vector2(
+                Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left"),
+                Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up")
+            ).LimitLength(1.0f);
+
+        Vector2 frictionAcceleration =
+            Velocity.MoveToward(Vector2.Zero, friction * (float)delta) - Velocity;
+
+        Vector2 acceleration = (userInputAcceleration + frictionAcceleration).LimitLength(
+            this.maxAcceleration * (float)delta
+        );
+
+        Velocity += acceleration;
+        Velocity = Velocity.LimitLength(maxSpeed);
+        MoveAndSlide();
+    }
+}
--- src/demos/diep/diepScripts/FaceMouse.cs
+++ src/demos/diep/diepScripts/FaceMouse.cs
@@ -3,20 +3,18 @@ using System;
 
 public partial class FaceMouse : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
-		PointToMouse();
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        PointToMouse();
+    }
 
-	private void PointToMouse() {
-		Vector2 mousePos = GetGlobalMousePosition();
-		Rotation = Mathf.Atan2(mousePos.Y - Position.Y, mousePos.X - Position.X);
-	}
+    private void PointToMouse()
+    {
+        Vector2 mousePos = GetGlobalMousePosition();
+        Rotation = Mathf.Atan2(mousePos.Y - GlobalPosition.Y, mousePos.X - GlobalPosition.X);
+    }
 }
