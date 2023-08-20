3c20570031860d7fa01c02cd089db8a1b85c93df
Jalen Morgan <jalen.morgan000@gmail.com>
2023-08-20 13:55:59 -0400

    Fix knockback direction

--- src/demos/demoPrefabs/CollisionDamage.cs
+++ src/demos/demoPrefabs/CollisionDamage.cs
@@ -6,12 +6,17 @@ public partial class CollisionDamage : Node
     [Export]
     public float damage = 10.0f;
 
+    [Export]
+    public float knockback = 100.0f;
+
     public void BodyEntered(Node2D other)
     {
-        GD.Print(other);
+        KinematicArea2D parent = this.GetParent() as KinematicArea2D;
         (other as Knockbackable)
             .GetKnockbackForce()
-            .ApplyForce(GodotSteeringAI.GSAIUtils.ToVector3(new Vector2(100.0f, 0.0f)));
+            .ApplyForce(
+                GodotSteeringAI.GSAIUtils.ToVector3(knockback * parent.LinearVelocity.Normalized())
+            );
 
         foreach (Health health in OpenTDE.Utils.GetChildrenOfType<Health>(other))
         {
