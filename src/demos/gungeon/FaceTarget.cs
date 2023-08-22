a018bdfdb42ac479d74a14bc3edd71d190c51f98
Jalen Morgan <jalen.morgan000@gmail.com>
2023-08-22 10:39:06 -0400

    Polish bow movement

--- src/demos/gungeon/FaceTarget.cs
+++ src/demos/gungeon/FaceTarget.cs
@@ -16,19 +16,18 @@ public partial class FaceTarget : Node2D
         steeringAgent = new GSAISteeringAgent();
         face = new GSAIFace(steeringAgent, target.targetLocation);
         targetAcceleration = new GSAITargetAcceleration();
-        GlobalRotation = GSAIUtils.Vector2ToAngle(
-            GSAIUtils.ToVector2(target?.targetLocation.Position ?? GSAIUtils.ToVector3(Position))
-                - Position
-        );
+        GlobalRotation = GSAIUtils.Vector2ToAngle(new Vector2(1.0f, 0.0f));
 
-        steeringAgent.AngularAccelerationMax = 10.0f;
-        steeringAgent.AngularSpeedMax = 100.0f;
+        face.DecelerationRadius = 20f;
+        face.AlignmentTolerance = 0.0f;
+        steeringAgent.AngularAccelerationMax = 200.0f;
+        steeringAgent.AngularSpeedMax = 250.0f;
     }
 
     public override void _PhysicsProcess(double _delta)
     {
         float delta = (float)_delta;
-        steeringAgent.AngularVelocity *= 0.95f;
+        steeringAgent.AngularVelocity *= 0.85f;
         steeringAgent.Orientation = GlobalRotation + Mathf.Pi / 2.0f;
         steeringAgent.Position = GSAIUtils.ToVector3(GlobalPosition);
 
