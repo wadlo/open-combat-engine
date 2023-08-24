c590717327dc0a68264e7f28bd5a694af7c120fd
Jalen Morgan <jalen.morgan000@gmail.com>
2023-08-24 07:45:42 -0400

    Add utility function

--- src/core/Utils.cs
+++ src/core/Utils.cs
@@ -41,5 +41,19 @@ namespace OpenTDE
 
             return closest;
         }
+
+        // Maps a float that is between [fromMin, fromMax] to the range [toMin, toMax].
+        // For example, MapFloat(10,20, 0,10, 15) => 5
+        public static float MapFloat(
+            float fromMin,
+            float fromMax,
+            float toMin,
+            float toMax,
+            float fromValue
+        )
+        {
+            float percent = (fromValue - fromMin) / (fromMax - fromMin);
+            return toMin + (toMax - toMin) * percent;
+        }
     }
 }
