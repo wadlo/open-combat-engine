7a882f481d5e13fc6e7b1a33ff8cd1a5d9228061
Jalen Morgan <jalen.morgan000@gmail.com>
2023-09-03 18:05:44 -0400

    Add unit color to battle scene

--- src/demos/clash/ClashSpawnPoint.cs
+++ src/demos/clash/ClashSpawnPoint.cs
@@ -26,6 +26,7 @@ public partial class ClashSpawnPoint : Node2D
         GetTree().Root.AddChild(instantiated);
 
         RandomlyPositionUnit(instantiated);
+        ColorUnit(instantiated);
     }
 
     public void SpawnSwordsman()
@@ -34,6 +35,19 @@ public partial class ClashSpawnPoint : Node2D
         GetTree().Root.AddChild(instantiated);
 
         RandomlyPositionUnit(instantiated);
+        ColorUnit(instantiated);
+    }
+
+    public void ColorUnit(CharacterBody2D instantiated)
+    {
+        (instantiated.GetChild<FaceTarget>(0).Material as ShaderMaterial).CallDeferred(
+            "set_shader_parameter",
+            "color",
+            unitColor
+        );
+
+        instantiated.GetChild<FaceTarget>(0).Modulate = unitColor;
+        instantiated.GetChild<FaceTarget>(0).SelfModulate = unitColor; // (unitColor + new Color(0, 0, 0)) / 2.0f;
     }
 
     public void RandomlyPositionUnit(CharacterBody2D instantiated)
